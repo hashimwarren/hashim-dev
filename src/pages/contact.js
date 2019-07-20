ac40333ad93c833590e677eb2ef32ad2018b2109
Hashim Warren <hashimwarren@gmail.com>
2019-07-20 18:41:21 -0400

    create contact and about page

---
+++ src/pages/contact.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const ContactPage = () => {
+    return (
+        <div>
+            <h1>Contact Hashim Warren</h1>
+            <p>email me at me@hashim.dev</p>
+        </div>
+
+    )
+}
+
+export default ContactPage
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,14 @@
-import React from "react"
+//react comes pre-installed with Gatsby
+import React from 'react'
 
-export default () => <div>Hello Greensboro, North Carolina!</div>
+const IndexPage = () => {
+    return (
+        <div>
+            <h1>Hello.</h1>
+            <p>I'm Hashim, a full stack marketer living in Greensboro</p>
 
+        </div>
+    )
+}
+
+export default IndexPage
