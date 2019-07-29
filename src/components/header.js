875253377ed1150a297bf759d20002a44b6c1667
Hashim Warren <hashimwarren@gmail.com>
2019-07-29 19:54:29 -0400

    complete lesson 7 - basic styling

--- src/components/header.js
+++ src/components/header.js
@@ -1,16 +1,24 @@
 import React from 'react'
 import { Link } from 'gatsby'
 
+//import './header.module.scss'
+
+import headerStyles from './header.module.scss'
+
 const Header = () => {
     return (
-        <header>
-            <h1>Hashim - Full Stack Marketer</h1>
+        <header className={headerStyles.header}>
+            <h1>
+            <Link className={headerStyles.title} to="/">
+                Hashim - Full Stack Marketer
+                </Link>
+            </h1>
             <nav>
-                <ul>
-                    <li><Link to='/'>Home</Link></li>
-                    <li><Link to='/about'>About</Link></li>
-                    <li><Link to='/contact'>Contact</Link></li>
-                    <li><Link to='/blog'>Blog</Link></li>
+                <ul className={headerStyles.navList}>
+                    <li><Link className={headerStyles.navItem} activeClassName={headerStyles.activeNavItem} to='/'>Home</Link></li>
+                    <li><Link className={headerStyles.navItem} activeClassName={headerStyles.activeNavItem} to='/about'>About</Link></li>
+                    <li><Link className={headerStyles.navItem} activeClassName={headerStyles.activeNavItem} to='/contact'>Contact</Link></li>
+                    <li><Link className={headerStyles.navItem} activeClassName={headerStyles.activeNavItem} to='/blog'>Blog</Link></li>
 
                 </ul>
             </nav>
