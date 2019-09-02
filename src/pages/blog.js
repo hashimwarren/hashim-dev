bbe54465d3aaa8947a9bc2dc8fcb2916ec1bf7a0
Hashim Warren <hashimwarren@gmail.com>
2019-09-02 17:50:42 -0400

    Lesson 11 - working with Markdown posts

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,18 +1,38 @@
 import React from "react"
 import Layout from "../components/layout"
-
-//
-// Goal: show list of posts on blog page
-// 1. Query the title and date for each post in a blog component
-//  2. Render and OL on the page
-// 3. Render a li with a nested h2 (title) and p (date) for each post  - "render array of objects"
-// 4. test your work
+import { graphql, useStaticQuery } from "gatsby"
 
 const BlogPage = () => {
+  const data = useStaticQuery(graphql`
+    query {
+      allMarkdownRemark {
+        edges {
+          node {
+            frontmatter {
+              title
+              date
+            }
+          }
+        }
+      }
+    }
+  `)
+
+  console.log(data)
   return (
     <Layout>
       <h1>Blog</h1>
       <p>Posts will show up here later on</p>
+      <ol>
+        {data.allMarkdownRemark.edges.map(edge => {
+          return (
+            <li>
+              <h2>{edge.node.frontmatter.title} </h2>
+              <p>{edge.node.frontmatter.date}</p>
+            </li>
+          )
+        })}
+      </ol>
     </Layout>
   )
 }
