b3c1bbf65c0d2e53dfd64ecd2c72aa362418cbc2
Hashim Warren <hashimwarren@gmail.com>
2019-10-09 17:11:54 -0400

    fix link to blog posts

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,6 +1,6 @@
 import React from "react"
 import Layout from "../components/layout"
-import { graphql, useStaticQuery } from "gatsby"
+import { Link, graphql, useStaticQuery } from "gatsby"
 
 import blogStyles from './blog.module.scss'
 
@@ -13,6 +13,9 @@ const BlogPage = () => {
             frontmatter {
               title
               date
+            }
+            fields {
+              slug
             }
           }
         }
@@ -29,8 +32,10 @@ const BlogPage = () => {
         {data.allMarkdownRemark.edges.map(edge => {
           return (
             <li className={blogStyles.post}>
-              <h2>{edge.node.frontmatter.title} </h2>
-              <p>{edge.node.frontmatter.date}</p>
+              <Link to={`/blog/${edge.node.fields.slug}`}>
+                <h2>{edge.node.frontmatter.title} </h2>
+                <p>{edge.node.frontmatter.date}</p>
+              </Link>
             </li>
           )
         })}
