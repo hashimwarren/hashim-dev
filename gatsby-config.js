3a94a60d49d2adef66cc3f342dc324fd8ea464aa
Hashim Warren <hashimwarren@gmail.com>
2019-10-15 16:24:30 -0400

    Add contentful env variables

--- gatsby-config.js
+++ gatsby-config.js
@@ -10,6 +10,14 @@ module.exports = {
     author: "Hashim Warren",
   },
   plugins: [
+    {
+      resolve: 'gatsby-source-contentful',
+      options: {
+        spaceId: 'process.env.CONTENTFUL_SPACE_ID',
+        accessToken: 'process.env.CONTENTFUL_ACCESS_TOKEN'
+      }
+
+    },
     "gatsby-plugin-sass",
     {
       resolve: "gatsby-source-filesystem",
