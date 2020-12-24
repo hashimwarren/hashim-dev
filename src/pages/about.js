488e869c791876acb399cde9da7c991dc50e81bd
Hashim Warren <hashimwarren@gmail.com>
2020-12-24 18:13:19 -0500

    Add link

--- src/pages/about.js
+++ src/pages/about.js
@@ -8,6 +8,7 @@ const AboutPage = () => {
         <Layout>
             <h1>About Hashim Warren</h1>
             <p>I was born in New York City on a Sunday...</p>
+        <p>I edit <a href="https://configmag.com/">Config</a>, where I track new products for building serverless websites</p>
             <p><Link to="/contact">Contact me</Link></p>
         </Layout>
 
